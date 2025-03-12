5d61e0bec70f6e42c26600baa59ed7c3c80cbd2c
Santosh Jain <santoshjain.id@gmail.com>
2025-03-13 00:13:37 +0530

    Rendering and Event Handling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import PackingList from './PackingList'
+import PackingListTernary from './PackingListTernary'
+import PackingListAnd from './PackingListAnd'
+import Button from './Button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+    <PackingList />
+    <PackingListTernary />
+    <PackingListAnd />
+    <Button />
+  </>
   );
 }
 

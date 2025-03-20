f11f52537ada5f6b2efd2be9b5635fd62cf8c29c
Santosh Jain <santosh.jain@timscdrmumbai.in>
2025-03-20 13:13:41 +0530

    changed folder structure

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import { useState } from 'react';
-import PackingList from './module-1/PackingList'
-import PackingListTernary from './module-1/PackingListTernary'
-import PackingListAnd from './module-1/PackingListAnd'
-import Button from './module-1/Button';
-import AlertButton from './module-1/AlertButton';
-import Increment from './module-2/Increment';
-import IncrementState from './module-2/IncrementState';
-import StateVariable from './module-2/StateVariable'
-import Timer from './module-2/Timer';
-import ThemeWindow from './module-2/ThemeWindow';
-import BasicForm from './module-2/BasicForm';
-import ReactForm from './module-2/ReactForm';
-import BasicReactForm from './module-2/BasicReactForm';
+import PackingList from './module-1/Conditional Rendering/PackingList'
+import PackingListTernary from './module-1/Conditional Rendering/PackingListTernary'
+import PackingListAnd from './module-1/Conditional Rendering/PackingListAnd'
+import Button from './module-1/Event/Button';
+import AlertButton from './module-1/Event/AlertButton';
+import Increment from './module-2/useState/Increment';
+import IncrementState from './module-2/useState/IncrementState';
+import StateVariable from './module-2/useState/StateVariable'
+import Timer from './module-2/useEffect/Timer';
+import ThemeWindow from './module-2/useContext/ThemeWindow';
+import BasicForm from './module-2/React-Form/BasicForm';
+import ReactForm from './module-2/React-Form/ReactForm';
+import BasicReactForm from './module-2/React-Form/BasicReactForm';
 
 
 function App() {
